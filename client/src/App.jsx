13039318ccda4d204775428dc44ef28bf9e6badb
darh.best <darh.best@gmail.com>
2025-10-13 15:00:35 +0200

    ClientBooking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ClientBooking from './pages/ClientBooking';
 
 function App() {
   const [count, setCount] = useState(0)
